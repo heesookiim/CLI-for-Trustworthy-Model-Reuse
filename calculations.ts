e7fd755bd69b9b2b35277bf6fbd8988861d5ae7c
Andrewh18 <111818262+hall657@users.noreply.github.com>
2023-09-10 19:41:50 -0400

    Updated to use objects to hold data for calculations and result from calculations

--- calculations.ts
+++ calculations.ts
@@ -1,71 +1,89 @@
+// included in fileio but included here to avoid errors
+// object to hold data for each module
+// remove once project is merged and this file has access to fileio.ts
+type module = {
+    link: string,
+    busFactor: number,
+    correctness: number,
+    rampUp: number,
+    responsiveMaintainer: number,
+    license: number,
+    netScore: number
+}
+
+// object to hold raw data for each module
+type data = {
+    contrubtorMostPullRequests: number, // Most active contributor's pull requests in past year, number
+    totalPullRequests: number,          // All pull requests in the last year, number
+    activeContributors: number,         // Number of active contributors in the past year, number
+    totalClosedIssues: number,          // number of total closed issues, number
+    totalissues: number,                // number of total issues, number
+    totalClosedIssuesMonth: number,     // number of closed issues in the last month, number
+    totalIssuesMonth: number,           // number of issues in the last month, number
+    quickStart: number,                 // existence of quickStart in README (0 for doesn't exist, 1 for exists)
+    examples: number,                   // existence of examples in README (0 for doesn't exist, 1 for exists)
+    usage: number                       // existance of usage section in README (0 for doesn't exist, 1 for exists)
+    closedIssues: number,               // number of closed issues in past 2 weeks, number
+    openIssues: number,                 // number of open issues, number
+    licenses: string[],                 // string of licenses for module and dependencies
+}
+
 // bus factor caclulation
-// inputs:
-// - contrubtorMostPullRequests: Most active contributor's pull requests in past year, number
-// - totalPullRequests: All pull requests in the last year, number
-// - activeContributors: Number of active contributors in the past year, number
+// input: raw data from REST API call
 // output: number from bus factor calculation [0, 1]
-function BusFactor(contrubtorMostPullRequests: number, totalPullRequests: number, activeContributors: number): number {
+function BusFactor(rawData: data): number {
     // check inputs for divide by 0
-    if(totalPullRequests == 0) {
+    if(rawData.totalPullRequests == 0) {
         return 0;
     }
 
-    let scaleFactor: number = Math.min(1, (activeContributors / 20));
-    let busFactor: number = 1 - (scaleFactor * contrubtorMostPullRequests / totalPullRequests);
+    let scaleFactor: number = Math.min(1, (rawData.activeContributors / 20));
+    let busFactor: number = 1 - (scaleFactor * rawData.contrubtorMostPullRequests / rawData.totalPullRequests);
     return busFactor;
 }
 
 // correctness calculation
-// inputs:
-// - totalClosedIssues: number of total closed issues, number
-// - totalissues: number of total issues, number
-// - totalClosedIssuesMonth: number of closed issues in the last month, number
-// - totalIssuesMonth: number of issues in the last month, number
+// input: raw data from REST API call
 // output: number from correctness calculation [0, 1]
-function Correctness(totalClosedIssues: number, totalissues: number, totalClosedIssuesMonth: number, totalIssuesMonth: number): number {
+function Correctness(rawData: data): number {
     // check inputs for divide by 0
-    if(totalissues == 0 || totalIssuesMonth == 0) {
+    if(rawData.totalissues == 0 || rawData.totalIssuesMonth == 0) {
         return 0;
     }
 
-    let totalRatio: number = totalClosedIssues / totalissues;
-    let monthRatio: number = totalClosedIssuesMonth / totalIssuesMonth;
+    let totalRatio: number = rawData.totalClosedIssues / rawData.totalissues;
+    let monthRatio: number = rawData.totalClosedIssuesMonth / rawData.totalIssuesMonth;
     return Math.min(totalRatio, monthRatio);
 }
 
 // ramp up calculation
-// inputs:
-// - quickStart: existence of quickStart in README (0 for doesn't exist, 1 for exists)
-// - examples: existence of examples in README (0 for doesn't exist, 1 for exists)
-// - usage: existance of usage section in README (0 for doesn't exist, 1 for exists)
+// input: raw data from REST API call
 // output: number from ramp up calculation [0, 1]
-function RampUp(quickStart: number, examples: number, usage: number): number {
-    return (0.5 * quickStart) + (0.25 * examples) + (0.25 * usage);
+function RampUp(rawData: data): number {
+    return (0.5 * rawData.quickStart) + (0.25 * rawData.examples) + (0.25 * rawData.usage);
 }
 
 // responsive maintainer calculation
-// inputs:
-// - closedIssues: number of closed issues in past 2 weeks, number
-// - openIssues: number of open issues, number
+// input: raw data from REST API call
 // output: number from responsive maintainer calculation [0, 1]
-function ResponsiveMaintainer(closedIssues: number, openIssues: number) {
+function ResponsiveMaintainer(rawData: data): number {
     // check inputs for divide by 0
-    if(openIssues == 0) {
+    if(rawData.openIssues == 0) {
         return 1;
     }
 
-    let issueRatio = closedIssues / openIssues;
+    let issueRatio = rawData.closedIssues / rawData.openIssues;
     return Math.min(issueRatio, 1);
 }
 
 // license calculation
-// input: list of licenses for the library and any dependencies, array of strings
+// input: raw data from REST API call
 // output: number from license calculation [0, 1]
-function License(licenses: string[]) {
+function License(rawData: data): number {
     let compliant: number = 1;  // compliance of license
     // check each license
-    for(let idx: number = 0; idx < licenses.length; idx++) {
-        if (licenses[idx] != 'GNU Lesser General Public License, version 2.1') {
+    for(let idx: number = 0; idx < (rawData.licenses).length; idx++) {
+        if ((rawData.licenses)[idx] != 'GNU Lesser General Public License, version 2.1') {
             compliant = 0;
         }
     }
@@ -74,23 +92,40 @@ function License(licenses: string[]) {
 }
 
 // net score calculation
-// inputs: 
-// - data: array of arrays with data for each calculation:
-//   [[contrubtorMostPullRequests: number, totalPullRequests: number, activeContributors: number],
-//    [totalClosedIssues: number, totalissues: number, totalClosedIssuesMonth: number, totalIssuesMonth: number]
-//    [quickStart: number, examples: number, usage: number]
-//    [closedIssues: number, openIssues: number]
-//    [licenses: string[]]]
-function NetScore(data: any[]): number {
-    // calculate each element of the net score
-    let busFactor: number = BusFactor(data[0[0]], data[0[1]], data[0[2]]);
-    let correctness: number = Correctness(data[1[0]], data[1[1]], data[1[2]], data[1[3]]);
-    let rampUp: number = RampUp(data[2[0]], data[2[1]], data[2[2]]);
-    let responsiveMaintainer: number = ResponsiveMaintainer(data[3[0]], data[3[1]]);
-    let license: number = License[data[4[0]]];
-
+// input: module with data from other metric calculations
+// output: number from net score calculation [0, 1]
+function NetScore(module: module): number {
     // calculate net score
-    let netScore: number = (0.4 * busFactor) + (0.15 * correctness) + (0.15 * rampUp) + (0.3 * responsiveMaintainer);
-    netScore = netScore - (1 * (1 - license));
-    return netScore;
+    return ((0.4 * module.busFactor) + (0.15 * module.correctness) + (0.15 * module.rampUp) + (0.3 * module.responsiveMaintainer))
+            - (1 * (1 - module.license))
+}
+
+// generate calculations for each module
+// input: array of modules with links filled in
+// output: array of modules with all fields filled
+function GenerateCalculations(moduleList: module[]): module[] {
+    let dataList: data[] = [];
+    // loop through each module and get data from REST API
+    // complete claculations for each module
+    for(let idx: number = 0; idx < moduleList.length; idx++) {
+        // only initialized below for testing calculations independently (can be changed for calculation testing)
+        // remove once REST API call is ready
+        let rawData: data = {contrubtorMostPullRequests: 0, totalPullRequests: 0, activeContributors: 0,
+                             totalClosedIssues: 0, totalissues: 0, totalClosedIssuesMonth: 0, totalIssuesMonth: 0,
+                             quickStart: 0, examples: 0, usage: 0, closedIssues: 0, openIssues: 0, licenses: []};
+
+        // call REST API on link from module
+        // example command:
+        // let rawData: data = RestAPI(moduleList[idx].link);
+
+        // calculate each metric and update module object
+        moduleList[idx].busFactor = BusFactor(rawData);
+        moduleList[idx].correctness = Correctness(rawData);
+        moduleList[idx].rampUp = RampUp(rawData);
+        moduleList[idx].responsiveMaintainer = ResponsiveMaintainer(rawData);
+        moduleList[idx].license = License(rawData);
+        moduleList[idx].netScore = NetScore(moduleList[idx]);
+    }
+
+    return moduleList;
 }
