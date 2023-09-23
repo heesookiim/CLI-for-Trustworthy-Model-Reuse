e1e7301faa03adef577cbeb03141bda0c9884e7a
atindewan <dewan2@purdue.edu>
2023-09-23 15:38:05 -0400

    fixing fetch.test.ts

--- src/fetch.test.ts
+++ src/fetch.test.ts
@@ -6,104 +6,55 @@ jest.mock('axios');
 
 // fetch_METRICS function
 test('should fetch and return MetricData', async () => {
-    // Mock Axios response
-    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({
-        status: 200,
-        data: [], // Provide sample data here
-    });
-
-    const apiLink = 'sample-api-link';
+    const apiLink = 'https://api.github.com/repos/cloudinary/cloudinary_npm';
     const result = await fetch_METRICS(apiLink);
 
-    expect(result).toEqual({
-        // Define your expected MetricData here based on the provided sample data
-    });
-});
-
-// fetch_METRICS function
-test('should handle API errors and return null', async () => {
-    // Mock Axios response for error
-    (axios.get as jest.MockedFunction<typeof axios.get>).mockRejectedValue(new Error('API error'));
-
-    const apiLink = 'sample-api-link';
-    const result = await fetch_METRICS(apiLink);
-
-    expect(result).toBeNull();
+    expect(result.totalPullers365).toBeGreaterThanOrEqual(0);
+    expect(result.mostPulls365).toBeGreaterThanOrEqual(0);
+    expect(result.totalPulls365).toBeGreaterThanOrEqual(0);
+    expect(result.issuesClosed).toBeGreaterThanOrEqual(0);
+    expect(result.issuesTotal).toBeGreaterThanOrEqual(0);
+    expect(result.issuesClosed30).toBeGreaterThanOrEqual(0);
+    expect(result.issuesTotal30).toBeGreaterThanOrEqual(0);
+    expect(result.issuesClosed14).toBeGreaterThanOrEqual(0);
+    expect(result.issuesOpen).toBeGreaterThanOrEqual(0);
 });
 
 //getLink function
 test('should extract GitHub repository link from npm package page', async () => {
-    // Mock Axios response with HTML containing GitHub repository link
-    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({
-        status: 200,
-        data: 'HTML content with GitHub repository link',
-    });
-
-    const npmLink = 'sample-npm-link';
+    const npmLink = 'https://www.npmjs.com/package/express';
     const result = await getLink(npmLink);
 
-    expect(result).toBe('sample-github-repo-link');
+    expect(result).toBe('https://github.com/expressjs/express');
 });
 
 //getLink function
 test('should return null when GitHub repository link is not found', async () => {
-    // Mock Axios response with HTML without GitHub repository link
-    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({
-        status: 200,
-        data: 'HTML content without GitHub repository link',
-    });
-
-    const npmLink = 'sample-npm-link';
+    const npmLink = 'https://www.npmjs.com/package1/express';
     const result = await getLink(npmLink);
 
-    expect(result).toBeNull();
+    expect(result).toBe(400);
 });
 
 // convertLink function
 test('should convert a GitHub link to the GitHub API link', () => {
-    const githubLink = 'sample-github-link';
+    const githubLink = 'https://github.com/cloudinary/cloudinary_npm';
     const result = convertLink(githubLink);
 
-    expect(result).toBe('https://api.github.com/repos/sample-owner/sample-repo');
+    expect(result).toBe('https://api.github.com/repos/cloudinary/cloudinary_npm');
 });
 
-//getLink function
-test('should handle HTTP errors and return null', async () => {
-    // Mock Axios response for HTTP error
-    (axios.get as jest.MockedFunction<typeof axios.get>).mockRejectedValue(new Error('HTTP error'));
-
-    const npmLink = 'sample-npm-link';
-    const result = await getLink(npmLink);
-
-    expect(result).toBeNull();
-});
-
-//convertLink function
 test('should convert a GitHub link to the GitHub API link', () => {
-    const githubLink = 'sample-github-link';
-    const result = convertLink(githubLink);
-
-    expect(result).toBe('https://api.github.com/repos/sample-owner/sample-repo');
+    const githubLink = 'https://api.github.com/repos/cloudinary/xyz';
+    const MetricDataPartial1 = {
+        issuesClosed: 0, // number of closed issues [correctness]
+        issuesTotal: 0, // total number of issues [correctness]
+        issuesClosed30: 0, // number of closed issues, last 30 days [correctness]
+        issuesTotal30: 0, // total number of issues, last 30 days [correctness]
+        issuesClosed14: 0, // number of closed issues, last 14 days [responsive maintainer]
+        issuesOpen: 0, // number of open issues [responsive maintainer]
+    };
+    const result = fetchIssues(githubLink, MetricDataPartial1);
+    expect(result).toBe(400);
 });
-
-//getLink function
-test('getLink should handle errors gracefully', async () => {
-    // Mock the Axios.get function to simulate an error (e.g., network error)
-    (axios.get as jest.MockedFunction<typeof axios.get>).mockRejectedValue(new Error('Network error'));
-  
-    // Call the getLink function
-    const result = await getLink('some-npm-link');
-  
-    // Assert that the function gracefully handles the error
-    expect(result).toBeNull();
-});
-
-// TODO: Add these tests
-// test fetch_Metric function to see if calls fetchi
-
-// while loop in fetch.ts line 59 // test while loop
-
-// respose.status === 200 // test if statement 
-
-// test try/catch line 238
 
