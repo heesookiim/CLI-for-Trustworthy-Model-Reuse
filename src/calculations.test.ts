cba652c68459f9a575c0a43b5748b0636606d806
atindewan <dewan2@purdue.edu>
2023-09-23 20:29:26 -0400

    test cases fixed

--- src/calculations.test.ts
+++ src/calculations.test.ts
@@ -1,14 +1,6 @@
 import { module } from './fileio';
 import { data, BusFactor, Correctness, RampUp, ResponsiveMaintainer, License, NetScore } from './calculations';
 
-// basic test for BusFactor
-// test('BusFactor', () => {
-//     let rawData: data = {contrubtorMostPullRequests: 10, totalPullRequests: 100, activeContributors: 20,
-//                          totalClosedIssues: 0, totalissues: 0, totalClosedIssuesMonth: 0, totalIssuesMonth: 0,
-//                          quickStart: 0, examples: 0, usage: 0, closedIssues: 0, openIssues: 0, licenses: []};
-//     expect(BusFactor(rawData)).toBe(0.9);
-// });
-
 // should return 0 is totalPullRequests is 0
 test('BusFactor0', () => {
     const rawData: data = {
--- src/fileio.test.ts
+++ src/fileio.test.ts
@@ -1,89 +1,90 @@
 import * as fs from 'fs';
 import { ReadFile, FindGitModules, FindNPMModules, FindOtherModules, GenerateOutput, URLFileHandler } from './fileio';
 
-describe('fileio.ts functions', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+beforeEach(() => {
+    jest.clearAllMocks();
+});
 
-    test('ReadFile should read and split file contents correctly', () => {
-        const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
-        mockReadFileSync.mockReturnValue('url1\nurl2\nurl3\n');
+test('ReadFile should read and split file contents correctly', () => {
+    const mockReadFileSync = jest.spyOn(fs, 'readFileSync');
+    mockReadFileSync.mockReturnValue('https://github.com/cloudinary/cloudinary_npm\nhttps://www.npmjs.com/package/express\nhttps://github.com/nullivex/nodist\nhttps://github.com/lodash/lodash\nhttps://www.npmjs.com/package/browserify');
 
-        const result = ReadFile('mocked-file-path');
+    const result = ReadFile('url.txt');
 
-        expect(result).toEqual(['url1', 'url2', 'url3']);
-        expect(mockReadFileSync).toHaveBeenCalledWith('mocked-file-path', 'utf-8');
-    });
+    expect(result).toEqual([
+        'https://github.com/cloudinary/cloudinary_npm',
+        'https://www.npmjs.com/package/express',
+        'https://github.com/nullivex/nodist',
+        'https://github.com/lodash/lodash',
+        'https://www.npmjs.com/package/browserify'
+    ]);
+});
 
-    test('FindGitModules should find GitHub modules correctly', () => {
-        const urls = ['url1', 'github.com/repo1', 'url2', 'github.com/repo2'];
-        const result = FindGitModules(urls);
+test('FindGitModules should find GitHub modules correctly', () => {
+    const urls = [
+        'https://github.com/cloudinary/cloudinary_npm',
+        'https://www.npmjs.com/package/express',
+        'https://github.com/nullivex/nodist'
+    ];
+    const result = FindGitModules(urls);
 
-        expect(result).toHaveLength(2);
-        expect(result).toContainEqual({ URL: 'github.com/repo1', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
-        expect(result).toContainEqual({ URL: 'github.com/repo2', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
-    });
+    expect(result).toHaveLength(2);
+    expect(result).toContainEqual({ URL: 'https://github.com/cloudinary/cloudinary_npm', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
+    expect(result).toContainEqual({ URL: 'https://github.com/nullivex/nodist', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
+});
 
-    test('FindGitModules should find GitHub modules correctly', () => {
-        const urls = ['url1', 'github.com/repo1', 'url2', 'github.com/repo2'];
-        const result = FindGitModules(urls);
+test('FindNPMModules should find NPM modules correctly', () => {
+    const urls = [
+        'https://github.com/cloudinary/cloudinary_npm',
+        'https://www.npmjs.com/package/express',
+        'https://www.npmjs.com/package/browserify'
+    ];
+    const result = FindNPMModules(urls);
 
-        expect(result).toHaveLength(2);
-        expect(result).toContainEqual({ URL: 'github.com/repo1', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
-        expect(result).toContainEqual({ URL: 'github.com/repo2', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
-    });
+    expect(result).toHaveLength(2);
+    expect(result).toContainEqual({ URL: 'https://www.npmjs.com/package/express', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
+    expect(result).toContainEqual({ URL: 'https://www.npmjs.com/package/browserify', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
+});
 
-    test('FindNPMModules should find NPM modules correctly', () => {
-        const urls = ['url1', 'npmjs.com/package1', 'url2', 'npmjs.com/package2'];
-        const result = FindNPMModules(urls);
+test('FindOtherModules should find other modules correctly', () => {
+    const urls = [
+        'https://github.com/cloudinary/cloudinary_npm', 
+        'other1.com',
+        'https://www.npmjs.com/package/express',
+        'https://www.npmjs.com/package/browserify', 
+        'other2.com'
+    ];
+    const result = FindOtherModules(urls);
 
-        expect(result).toHaveLength(2);
-        expect(result).toContainEqual({ URL: 'npmjs.com/package1', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
-        expect(result).toContainEqual({ URL: 'npmjs.com/package2', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
-    });
+    expect(result).toHaveLength(2);
+    expect(result).toContainEqual({ URL: 'other1.com', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
+    expect(result).toContainEqual({ URL: 'other2.com', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
+});
 
-    test('FindOtherModules should find other modules correctly', () => {
-        const urls = ['url1', 'other1.com', 'url2', 'other2.com'];
-        const result = FindOtherModules(urls);
+test('GenerateOutput should generate output correctly', () => {
+    // Mock the ndjson.stringify function
+    const originalNdjsonStringify = require('ndjson').stringify;
+    const mockNdjsonStringify = jest.fn();
+    require('ndjson').stringify = mockNdjsonStringify;
 
-        expect(result).toHaveLength(2);
-        expect(result).toContainEqual({ URL: 'other1.com', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
-        expect(result).toContainEqual({ URL: 'other2.com', NET_SCORE: 0, RAMP_UP_SCORE: 0, CORRECTNESS_SCORE: 0, BUS_FACTOR_SCORE: 0, RESPONSIVE_MAINTAINER_SCORE: 0, LICENSE_SCORE: 0 });
-    });
+    // Call GenerateOutput with module data
+    const moduleData = {
+        URL: 'github.com/test-repo',
+        NET_SCORE: 0.75,
+        RAMP_UP_SCORE: 0.8,
+        CORRECTNESS_SCORE: 0.9,
+        BUS_FACTOR_SCORE: 0.7,
+        RESPONSIVE_MAINTAINER_SCORE: 0.85,
+        LICENSE_SCORE: 1,
+    };
+    GenerateOutput(moduleData);
 
-    test('GenerateOutput should generate output correctly', () => {
-        // Mock the ndjson.stringify() function
-        const mockNdjsonStringify = jest.fn();
-        mockNdjsonStringify.mockReturnValue({
-            pipe: jest.fn(),
-            write: jest.fn(),
-            end: jest.fn(),
-        });
+    // Check that ndjson.stringify was called correctly
+    expect(mockNdjsonStringify).toHaveBeenCalledWith();
+    expect(mockNdjsonStringify().pipe).toHaveBeenCalledWith(process.stdout);
+    expect(mockNdjsonStringify().write).toHaveBeenCalledWith(moduleData);
+    expect(mockNdjsonStringify().end).toHaveBeenCalledWith();
 
-        const originalNdjsonStringify = require('ndjson').stringify;
-        require('ndjson').stringify = mockNdjsonStringify;
-
-        // Call GenerateOutput
-        const moduleData = {
-            URL: 'github.com/test-repo',
-            NET_SCORE: 0.75,
-            RAMP_UP_SCORE: 0.8,
-            CORRECTNESS_SCORE: 0.9,
-            BUS_FACTOR_SCORE: 0.7,
-            RESPONSIVE_MAINTAINER_SCORE: 0.85,
-            LICENSE_SCORE: 1,
-        };
-
-        GenerateOutput(moduleData);
-
-        // Check that ndjson.stringify was called correctly
-        expect(mockNdjsonStringify).toHaveBeenCalledWith();
-        expect(mockNdjsonStringify().pipe).toHaveBeenCalledWith(process.stdout);
-        expect(mockNdjsonStringify().write).toHaveBeenCalledWith(moduleData);
-        expect(mockNdjsonStringify().end).toHaveBeenCalledWith();
-
-        // Restore the original ndjson.stringify function
-        require('ndjson').stringify = originalNdjsonStringify;
-    });
+    // Restore the original ndjson.stringify function
+    require('ndjson').stringify = originalNdjsonStringify;
 });
